30a1be9fdb7101f97e66f5563f6e9893cf88d654
Marissa <Marissa@UtterbergDataDev.com>
2018-09-11 14:29:01 -0400

    Update pelicanconf.py
    Changed links & social widget names.

--- pelicanconf.py
+++ pelicanconf.py
@@ -36,3 +36,6 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
+
+LINKS_WIDGET_NAME = 'Other projects by Utterberg Data & Development'
+SOCIAL_WIDGET_NAME = 'Connect'
