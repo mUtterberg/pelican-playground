89243b8178e79ef66ef54714629a2fb649b9ff9e
mutterberg <Marissa@UtterbergDataDev.com>
2018-10-09 11:53:33 -0400

    Configurations for Flex theme.

--- pelicanconf.py
+++ pelicanconf.py
@@ -8,11 +8,29 @@ AUTHOR = 'Marissa Utterberg'
 SITENAME = 'My Pelican Playground'
 SITESUBTITLE = 'A #100DaysOfCode Project'
 SITEURL = 'https://mutterberg.github.io/pelican-playground'
+SITELOGO = SITEURL + '/content/images/logo.jpg'
+
+BROWSER_COLOR = '#810016'
+
+CC_LICENSE = {
+    'name': 'Creative Commons Attribution-ShareAlike',
+    'version': '4.0',
+    'slug': 'by-sa'
+}
+
+COPYRIGHT_YEAR = 2018
+
+EXTRA_PATH_METADATA = {
+    'extra/custom.css': {'path': 'static/custom.css'},
+}
+CUSTOM_CSS = 'static/custom.css'
+
+MAIN_MENU = True
 
 PATH = 'content'
 
 TIMEZONE = 'America/New_York'
-
+OC_LOCALE = 'en_US'
 DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
