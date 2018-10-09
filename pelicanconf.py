361d86b0709a0f34e0cb2a36194591c60a0770fd
mutterberg <Marissa@UtterbergDataDev.com>
2018-10-09 12:21:24 -0400

    Remove import reference to deleted plugin

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
-from plugins import always_modified, custom
+from plugins import always_modified
 
 THEME = 'Flex'
 
